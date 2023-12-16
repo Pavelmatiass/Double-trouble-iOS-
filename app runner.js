7d51d182bf493612f3618ad4d1974538e0d56796
Pavel Matiass <154022091+Pavelmatiass@users.noreply.github.com>
2023-12-16 22:40:43 +0200

    Create app runner.js

---
+++ app runner.js
@@ -0,0 +1,10 @@
+    "event_info": {
+      "log_message": "string",
+      "request_method": "string",
+      "request_url": "string",
+      "referrer": "string",
+      "user_agent": "string",
+      "service": "string",
+      "version": "string",
+      "response_status": "string"
+    }
