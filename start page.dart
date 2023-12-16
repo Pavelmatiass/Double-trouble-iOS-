f564f7c7c75f5fa73f6a6f5e5672658d77bbf0be
Pavel Matiass <154022091+Pavelmatiass@users.noreply.github.com>
2023-12-16 22:38:54 +0200

    Create start page.dart

---
+++ start page.dart
@@ -0,0 +1,17 @@
+library pylons_sdk;
+
+export 'src/core/error/exceptions.dart';
+
+export 'src/pylons_wallet.dart';
+export 'src/types/cookbook.dart';
+export 'src/types/execution.dart'
+
+import 'dart:async' as _i3;
+
+import 'package:mockito/mockito.dart' as _i1;
+import 'package:uni_links_platform_interface/uni_links_platform_interface.dart'
+    as _i2;
+  @override
+  _i3.Stream<String?> get linkStream =>
+      (super.noSuchMethod(Invocation.getter(#linkStream),
+          returnValue: Stream<String?>.empty()) as _i3.Stream<String?>);
