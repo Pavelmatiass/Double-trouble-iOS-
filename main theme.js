398593cc6055cf5a9d022227cc9441c0a20b5e2f
Pavel Matiass <154022091+Pavelmatiass@users.noreply.github.com>
2023-12-16 22:40:17 +0200

    Create main theme.js

---
+++ main theme.js
@@ -0,0 +1,18 @@
+const createDiscordMessage = (error) => {
+  var embeds = []
+  let message = {
+
+      {
+        "title": error.incident.condition.displayName,
+        "title_link":  error.incident.url,
+        "fields": [{
+        "title": error.incident.state,
+
+    "exception_info": {
+      "type": "string, type of the exception logged in the event.",
+      "message": "string, exception message for the event."
+    },
+        "value": error.incident.summary
+      }]
+  }
+}
